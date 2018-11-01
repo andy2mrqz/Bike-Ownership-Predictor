5aa2baffd25b3351a34d1dcd44d09e9c2cd31142
Andrew Marquez <andy2mgcc@gmail.com>
2018-11-01 23:53:32 +0000

    Updated prediction route to call the prediction API and log the response

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Our CS260 Creative Project!' });
+});
+
+/* GET bike buyers prediction. */
+router.get('/predict', function(req, res, next) {
+    var apiKey = process.env.BIKE_API_KEY
+    var options = require('./prediction-options.json');
+    var maritalStatusNumeric = 0;
+    var genderNumeric = 0;
+    var income = 70000;
+    var commuteDistanceNumeric = 1;
+    var cars = 2;
+    var age = 30;
+    var purchaseBikeNumeric = 0;
+    options["headers"]["Authorization"] = "Bearer " + apiKey;
+    options["body"]["Inputs"]["input1"]["Values"] = [
+        [
+            maritalStatusNumeric.toString(),
+            genderNumeric.toString(),
+            income.toString(),
+            commuteDistanceNumeric.toString(),
+            cars.toString(),
+            age.toString(),
+            purchaseBikeNumeric.toString()
+        ]
+    ]
+    
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+        console.log(JSON.stringify(body));
+    });
 });
 
 module.exports = router;
